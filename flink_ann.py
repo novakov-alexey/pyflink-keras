1376b3852efd92d63b9eb2fc71546d829277d0e5
Alexey Novakov <mein-eingang@eclipso.eu>
2025-01-18 19:27:19 +0100

    add script arguments

--- flink_ann.py
+++ flink_ann.py
@@ -4,6 +4,7 @@ import pandas as pd
 import argparse
 import logging
 import sys
+import argparse
 
 from sklearn.preprocessing import LabelEncoder
 from sklearn.compose import ColumnTransformer
@@ -17,12 +18,13 @@ from pyflink.table.expressions import col, range_, with_columns
 from pyflink.common import Row, Configuration
 
 class Predict(ScalarFunction):
-    def __init__(self, transform):
+    def __init__(self, transform, kerasModelPath: str):
         self.transform = transform
+        self.kerasModelPath = kerasModelPath
 
     def open(self, func_context):
         import tensorflow as tf        
-        self.ann = tf.keras.models.load_model('saved_model/ann-customer-churn.keras')        
+        self.ann = tf.keras.models.load_model(self.kerasModelPath)        
 
     def eval(self, *args):        
         a = np.array([args])
@@ -30,8 +32,7 @@ class Predict(ScalarFunction):
         prediction = self.ann.predict(np.array([features]))[0, 0]        
         return Row(raw_prediction=prediction, exited=prediction > 0.5)
 
-def churn_analysis():
-    trainDataPath = './Churn_Modelling.csv'
+def churn_analysis(trainDataPath: str, liveDataPath: str, kerasModelPath: str):    
     dataset = pd.read_csv(trainDataPath)
     features = dataset.iloc[:, 3:-1].values
 
@@ -67,17 +68,16 @@ def churn_analysis():
               .column("EstimatedSalary", DataTypes.DOUBLE())
               .column("Exited", DataTypes.DOUBLE())              
               .build())
-    
-    testDataPath = './test_data/Churn_Modelling.csv'
+        
     tableDesc = (TableDescriptor
                 .for_connector('filesystem')
                 .schema(schema)
-                .option("path", testDataPath)
+                .option("path", liveDataPath)
                 .option("format", "csv")
                 .option("csv.allow-comments", "true")
                 .build())
     
-    predict = udf(Predict(transform), result_type=DataTypes.ROW(
+    predict = udf(Predict(transform, kerasModelPath), result_type=DataTypes.ROW(
         [
             DataTypes.FIELD('raw_prediction', DataTypes.FLOAT()),
             DataTypes.FIELD('exited', DataTypes.BOOLEAN())
@@ -93,4 +93,29 @@ def churn_analysis():
 
 if __name__ == '__main__':
     logging.basicConfig(stream=sys.stdout, level=logging.DEBUG, format="%(message)s")
-    churn_analysis()
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--extractor-train-file',
+        dest='trainDataPath',
+        required=False,
+        default='./Churn_Modelling.csv',
+        help='Path to CSV trainining file for the Feature Exractor')
+    parser.add_argument(
+        '--live-data-file',
+        dest='liveDataPath',
+        required=False,
+        default='./test_data/Churn_Modelling.csv',
+        help='Path to CSV live data file for model inference')
+        
+    parser.add_argument(
+        '--keras-model-file',
+        dest='kerasModelPath',
+        required=False,
+        default='./saved_model/ann-customer-churn.keras',
+        help='Path to Kearas Model file')
+
+    argv = sys.argv[1:]
+    known_args, _ = parser.parse_known_args(argv)
+    
+    churn_analysis(known_args.trainDataPath, known_args.liveDataPath, known_args.kerasModelPath)
